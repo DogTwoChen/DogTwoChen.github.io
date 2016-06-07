3083d4e254f0e5ed84bdf7daabf2bf73333680d7
cjj1993316 <197137496@qq.com>
2016-06-07 15:13:55 +0800

    update

---
+++ js/photo.js
@@ -0,0 +1,4 @@
+li += '<li><div class="img-box">' +
+                       '<a class="img-bg" rel="example_group" href="https://github.com/lwzhang/blog/blob/master/photos/' + data[i] + '?raw=true"></a>' +
+                       '<img lazy-src="http://7xv5m6.com1.z0.glb.clouddn.com/' + data[i] + '?raw=true" />' +
+                       '</li>';
